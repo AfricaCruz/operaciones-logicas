9c857b7a1a11e510a2765e6fc8c880517fe90ba8
AfricaCruz <africacruzzconde@gmail.com>
2025-03-06 09:14:31 +0100

    cc

---
+++ script.js
@@ -0,0 +1,22 @@
+function evaluarLogica() {
+    const valor1 = parseInt(document.getElementById('valor1').value);
+    const valor2 = parseInt(document.getElementById('valor2').value);
+    const operador = document.getElementById('operador').value;
+    let resultado;
+
+    switch (operador) {
+        case 'AND':
+            resultado = valor1 & valor2;
+            break;
+        case 'OR':
+            resultado = valor1 | valor2;
+            break;
+        case 'XOR':
+            resultado = valor1 ^ valor2;
+            break;
+        default:
+            resultado = 'Operador no válido';
+    }
+
+    document.getElementById('resultado').innerText = `Resultado: ${resultado}`;
+}
